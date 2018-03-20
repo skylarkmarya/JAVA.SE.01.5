11bea7a236dd65c87697da2022a97d9c6ac5c344
pcsaic <pcsaic@yandex.ru>
2018-03-20 11:32:42 +0300

    JAVA.SE.01.5 task

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter N:");
+        int N = Integer.parseInt(reader.readLine());
+        int arr[][] = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = ( (i==j || N-i==j+1) ? 1 : 0 );
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
